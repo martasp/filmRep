89292478e6db42984973279f1e5ada01064c43ec
Martynas Pocius <Martasp3289@gmail.com>
2017-08-07 18:44:47 +0300

    Final

--- ClientApp/components/Home.tsx
+++ ClientApp/components/Home.tsx
@@ -3,7 +3,12 @@ import * as React from 'react';
 export class Home extends React.Component<{}, {}> {
     public render() {
         return <div>
-            <h1>Welcome to film api</h1>
+            <h1>Welcome to film api
+                (This is coded with .net core and react with Typescript)</h1>
+            <h2>functionality</h2>
+            <h3>Add movies</h3>
+            <h3>Add actors</h3>
+            <h3>Sorting</h3>
         </div>;
     }
 }
--- ClientApp/routes.tsx
+++ ClientApp/routes.tsx
@@ -2,14 +2,10 @@ import * as React from 'react';
 import { Route } from 'react-router-dom';
 import { Layout } from './components/Layout';
 import { Home } from './components/Home';
-import { FetchData } from './components/FetchData';
-import { Counter } from './components/Counter';
 import { MovieAdd } from './components/MovieAdd';
 import { Movies } from './components/Movies';
 export const routes = <Layout>
     <Route exact path='/' component={ Home } />
-    <Route path='/counter' component={ Counter } />
-    <Route path='/fetchdata' component={FetchData} />
     <Route path='/MovieAdd' component={MovieAdd} />
     <Route path='/Movies' component={Movies} />
 </Layout>;
